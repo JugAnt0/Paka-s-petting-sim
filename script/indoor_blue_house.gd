bbf74588f15871a0d69c469dfe6a222ed51af59f
JugAnt0 <canaletwomeyantonio@gmail.com>
2025-11-18 18:54:33 -0500

    Added two more houses, their interior, their lumination and their code. Re worked house menu, (still needs work),added more furniture sprites

---
+++ script/indoor_blue_house.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _ready() -> void:
+	$fade_transition/AnimationPlayer.play("fade_out")
--- script/start_game.gd
+++ script/start_game.gd
@@ -23,13 +23,21 @@ func _on_blue_button_pressed() -> void:
 	$fade_transition/AnimationPlayer.play("fade_in")
 
 
+
+
+func _on_yellow_button_pressed() -> void:
+	button_type = "yellow"
+	$fade_transition.show()
+	$fade_transition/FadeTimer.start()
+	$fade_transition/AnimationPlayer.play("fade_in")
+
+
 func _on_fade_timer_timeout() -> void:
 	if button_type == "red":
 		get_tree().change_scene_to_file("res://scenes/indoor_red_house.tscn")
 		
 	elif button_type == "blue":
 		get_tree().change_scene_to_file("res://scenes/indoor_blue_house.tscn")
-
-
-
-	
+		
+	elif button_type == "yellow":
+		get_tree().change_scene_to_file("res://scenes/edificio_indoor.tscn")
